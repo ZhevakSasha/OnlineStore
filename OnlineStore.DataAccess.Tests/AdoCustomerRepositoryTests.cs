247e4e6f0c7a365df4598d2dfce051c8715d375a
Sasha <89193419+ZhevakSasha@users.noreply.github.com>
2021-09-02 15:56:04 +0300

    Creating NUnit test for GetEntity method/

--- OnlineStore.DataAccess.Tests/AdoCustomerRepositoryTests.cs
+++ OnlineStore.DataAccess.Tests/AdoCustomerRepositoryTests.cs
@@ -1,23 +1,28 @@
 using NUnit.Framework;
 using OnlineStore.DataAccess.AdoRepositoryImplementation;
+using OnlineStore.DataAccess.DataModel;
 
 namespace OnlineStore.DataAccess.Tests
 {
     public class Tests
     {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=OnlineStoreDataBAse;Integrated Security=True";
+        AdoCustomerRepository customer1;
 
-        AdoCustomerRepository customer;
 
         [SetUp]
         public void Setup()
         {
-            customer = new AdoCustomerRepository(@"Data Source=(LocalDb)\MSSQLLocalDB;database=OnlineStoreDataBase4.dbo;trusted_connection=yes;");
+           // customer = new AdoCustomerRepository(@"Data Source=(LocalDb)\MSSQLLocalDB;database=OnlineStoreDataBase4.dbo;trusted_connection=yes;");
         }
 
         [Test]
-        public void AdoRepositoryImplementationTest()
+        public void GetEntityTest()
         {
-            Assert.Pass();
+            //customer1 = new AdoCustomerRepository(@"Data Source=(LocalDb)\MSSQLLocalDB;database=OnlineStoreDataBase4.dbo;trusted_connection=True;");
+            
+            customer1 = new AdoCustomerRepository(connectionString);
+            Assert.AreEqual(customer1.GetEntity(1).FirstName,"Sasha");
         }
     }
 }
