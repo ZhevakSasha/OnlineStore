db54e32b71f3cf325f819292f385324b346619d8
Sasha <89193419+ZhevakSasha@users.noreply.github.com>
2021-09-03 16:07:45 +0300

    Creating json file.

--- OnlineStore.DataAccess.Tests/AdoCustomerRepositoryTests.cs
+++ OnlineStore.DataAccess.Tests/AdoCustomerRepositoryTests.cs
@@ -1,28 +1,41 @@
 using NUnit.Framework;
 using OnlineStore.DataAccess.AdoRepositoryImplementation;
 using OnlineStore.DataAccess.DataModel;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace OnlineStore.DataAccess.Tests
 {
     public class Tests
     {
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=OnlineStoreDataBAse;Integrated Security=True";
-        AdoCustomerRepository customer1;
+        IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile(path: "appconfig.json")
+                .Build();
+        //string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OnlineStoreDataBase4;Integrated Security=True";
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+
 
 
         [SetUp]
         public void Setup()
         {
-           // customer = new AdoCustomerRepository(@"Data Source=(LocalDb)\MSSQLLocalDB;database=OnlineStoreDataBase4.dbo;trusted_connection=yes;");
+          
         }
 
         [Test]
-        public void GetEntityTest()
+        public void Get_CustomerById_ReturnsFirstName()
         {
-            //customer1 = new AdoCustomerRepository(@"Data Source=(LocalDb)\MSSQLLocalDB;database=OnlineStoreDataBase4.dbo;trusted_connection=True;");
-            
-            customer1 = new AdoCustomerRepository(connectionString);
-            Assert.AreEqual(customer1.GetEntity(1).FirstName,"Sasha");
+            //Arrange
+            AdoCustomerRepository customer = new AdoCustomerRepository(configuration["DefaultConnection"]);
+
+
+            //Act
+            var actual = customer.GetEntity(1).FirstName;
+
+            //Assert
+            Assert.AreEqual(actual,"Sasha");
         }
     }
 }
--- OnlineStore.DataAccess/DataAccess/DataBaseContext.cs
+++ OnlineStore.DataAccess/DataAccess/DataBaseContext.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using OnlineStore.DataAccess.DataModel;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace OnlineStore.DataAccess.DataAccess
 {
    public class DataBaseContext : DbContext 
     {
         protected DataBaseContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["Def"].ConnectionString);
+        }
 
         public DbSet<Customer> Customers { get; set; }
 
