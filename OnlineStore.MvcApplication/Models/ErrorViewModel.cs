a91216b8ae44bd292089890e69f275a05ea734d5
Sasha <89193419+ZhevakSasha@users.noreply.github.com>
2021-09-21 16:02:40 +0300

    Creating Business layer and Mvc applications projects

---
+++ OnlineStore.MvcApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineStore.MvcApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
