38ef0e905f2171e3508dbc7bdd13b6b501543dba
Sasha <89193419+ZhevakSasha@users.noreply.github.com>
2021-10-20 15:56:58 +0300

    Creating IdentityApi solution

---
+++ OnlineStore.IdentityApi/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.IdentityApi
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
